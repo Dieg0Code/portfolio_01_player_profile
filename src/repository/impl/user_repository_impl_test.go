2b8474f54bed78bcbf0a8233d0dedb54011fcaf6
dieg0code <diegoobando20@gmail.com>
2024-07-18 15:31:48 -0400

    Error test cases added to user_repository

--- src/repository/impl/user_repository_impl_test.go
+++ src/repository/impl/user_repository_impl_test.go
@@ -9,144 +9,230 @@ import (
 )
 
 func TestUserRepositoryImpl_CreateUser(t *testing.T) {
+	t.Run("CreateUser_Success", func(t *testing.T) {
+		// Setup
+		db := testutils.SetupTestDB(&models.User{}, &models.PlayerProfile{})
+		defer func() {
+			sqlDB, _ := db.DB()
+			sqlDB.Close()
+		}()
 
-	// Setup
-	db := testutils.SetupTestDB(&models.User{}, &models.PlayerProfile{})
-	defer func() {
-		sqlDB, _ := db.DB()
-		sqlDB.Close()
-	}()
+		// Create repository instance
+		repo := NewUserRepositoryImpl(db)
 
-	// Create repository instance
-	repo := NewUserRepositoryImpl(db)
+		// Define user to be created
+		user := &models.User{
+			UserName: "test",
+			PassWord: "test",
+			Email:    "test@test.com",
+			Age:      20,
+			Profiles: []models.PlayerProfile{},
+		}
 
-	// Define user to be created
-	user := &models.User{
-		UserName: "test",
-		PassWord: "test",
-		Email:    "test@test.com",
-		Age:      20,
-		Profiles: []models.PlayerProfile{},
-	}
+		// Attempt to create user
+		require.NoError(t, repo.CreateUser(user), "Error creating user")
 
-	// Attempt to create user
-	require.NoError(t, repo.CreateUser(user), "Error creating user")
+		// Verify user was created
+		var dbUser models.User
+		require.NoError(t, db.Preload("Profiles").First(&dbUser, "user_name = ?", user.UserName).Error, "Error getting user")
 
-	// Verify user was created
-	var dbUser models.User
-	require.NoError(t, db.Preload("Profiles").First(&dbUser, "user_name = ?", user.UserName).Error, "Error getting user")
+		// Assertions
+		require.Equal(t, user.UserName, dbUser.UserName, "Usernames do not match")
+	})
 
-	// Assertions
-	require.Equal(t, user.UserName, dbUser.UserName, "Usernames do not match")
+	t.Run("CreateUser_Duplicate", func(t *testing.T) {
+		db := testutils.SetupTestDB(&models.User{}, &models.PlayerProfile{})
+		defer func() {
+			sqlDB, _ := db.DB()
+			sqlDB.Close()
+		}()
+
+		repo := NewUserRepositoryImpl(db)
+
+		user := &models.User{
+			UserName: "test",
+			PassWord: "test",
+			Email:    "test@test.com",
+			Age:      20,
+			Profiles: []models.PlayerProfile{},
+		}
+
+		require.NoError(t, repo.CreateUser(user), "Error creating user")
+
+		// Attempt to create same user again
+		err := repo.CreateUser(user)
+		require.Error(t, err, "Expected error creating duplicate user")
+	})
 }
 
 func TestUserRepositoryImpl_GetUser(t *testing.T) {
 
-	// Setup
-	db := testutils.SetupTestDB(&models.User{}, &models.PlayerProfile{})
-	defer func() {
-		sqlDB, _ := db.DB()
-		sqlDB.Close()
-	}()
+	t.Run("GetUser_Success", func(t *testing.T) {
+		// Setup
+		db := testutils.SetupTestDB(&models.User{}, &models.PlayerProfile{})
+		defer func() {
+			sqlDB, _ := db.DB()
+			sqlDB.Close()
+		}()
 
-	// Create repository instance
-	repo := NewUserRepositoryImpl(db)
+		// Create repository instance
+		repo := NewUserRepositoryImpl(db)
 
-	// Define user to be created
-	user := &models.User{
-		UserName: "test",
-		PassWord: "test",
-		Email:    "test@test.com",
-		Age:      20,
-		Profiles: []models.PlayerProfile{},
-	}
+		// Define user to be created
+		user := &models.User{
+			UserName: "test",
+			PassWord: "test",
+			Email:    "test@test.com",
+			Age:      20,
+			Profiles: []models.PlayerProfile{},
+		}
 
-	// Attempt to create user
-	require.NoError(t, repo.CreateUser(user), "Error creating user")
+		// Attempt to create user
+		require.NoError(t, repo.CreateUser(user), "Error creating user")
 
-	// Verify user was created
-	require.NotZero(t, user.UserID, "User ID is zero")
+		// Verify user was created
+		require.NotZero(t, user.UserID, "User ID is zero")
 
-	// Attempt to get user
-	dbUser, err := repo.GetUser(user.UserID)
-	require.NoError(t, err, "Error getting user")
+		// Attempt to get user
+		dbUser, err := repo.GetUser(user.UserID)
+		require.NoError(t, err, "Error getting user")
 
-	// Assertions
-	require.Equal(t, user.UserName, dbUser.UserName, "Usernames do not match")
+		// Assertions
+		require.Equal(t, user.UserName, dbUser.UserName, "Usernames do not match")
+	})
+
+	t.Run("GetUser_NotFount", func(t *testing.T) {
+		// Setup
+		db := testutils.SetupTestDB(&models.User{}, &models.PlayerProfile{})
+		defer func() {
+			sqlDB, _ := db.DB()
+			sqlDB.Close()
+		}()
+
+		repo := NewUserRepositoryImpl(db)
+
+		// Attempt to get a non-existent user
+		_, err := repo.GetUser(9999) // Assume 9999 is a non-existent ID
+		require.Error(t, err, "Expected error when getting non-existent user")
+	})
 }
 
 func TestUserRepositoryImpl_UpdateUser(t *testing.T) {
 
-	// Setup
-	db := testutils.SetupTestDB(&models.User{}, &models.PlayerProfile{})
-	defer func() {
-		sqlDB, _ := db.DB()
-		sqlDB.Close()
-	}()
+	t.Run("UpdateUser_Success", func(t *testing.T) {
+		// Setup
+		db := testutils.SetupTestDB(&models.User{}, &models.PlayerProfile{})
+		defer func() {
+			sqlDB, _ := db.DB()
+			sqlDB.Close()
+		}()
 
-	// Create repository instance
-	repo := NewUserRepositoryImpl(db)
+		// Create repository instance
+		repo := NewUserRepositoryImpl(db)
 
-	// Define user to be created
-	user := &models.User{
-		UserName: "test",
-		PassWord: "test",
-		Email:    "test@test.com",
-		Age:      20,
-		Profiles: []models.PlayerProfile{},
-	}
+		// Define user to be created
+		user := &models.User{
+			UserName: "test",
+			PassWord: "test",
+			Email:    "test@test.com",
+			Age:      20,
+			Profiles: []models.PlayerProfile{},
+		}
 
-	// Attempt to create user
-	require.NoError(t, repo.CreateUser(user), "Error creating user")
+		// Attempt to create user
+		require.NoError(t, repo.CreateUser(user), "Error creating user")
 
-	// Update user
-	user.Email = "UPDATED"
+		// Update user
+		user.Email = "UPDATED"
 
-	// Attempt to update user
-	require.NoError(t, repo.UpdateUser(user), "Error updating user")
+		// Attempt to update user
+		require.NoError(t, repo.UpdateUser(user), "Error updating user")
 
-	// Verify user was updated
-	var dbUser models.User
-	require.NoError(t, db.Preload("Profiles").First(&dbUser, "user_name = ?", user.UserName).Error, "Error getting user")
+		// Verify user was updated
+		var dbUser models.User
+		require.NoError(t, db.Preload("Profiles").First(&dbUser, "user_name = ?", user.UserName).Error, "Error getting user")
 
-	// Assertions
-	require.Equal(t, user.Email, dbUser.Email, "Emails do not match")
+		// Assertions
+		require.Equal(t, user.Email, dbUser.Email, "Emails do not match")
+	})
+
+	t.Run("UpdateUser_NotFound", func(t *testing.T) {
+		// Setup
+		db := testutils.SetupTestDB(&models.User{}, &models.PlayerProfile{})
+		defer func() {
+			sqlDB, _ := db.DB()
+			sqlDB.Close()
+		}()
+
+		repo := NewUserRepositoryImpl(db)
+
+		// Create a new user instance with a non-existent ID
+		nonExistentUser := &models.User{
+			UserID:   9999, // Assume 9999 is a non-existent ID
+			UserName: "nonexistent",
+			PassWord: "test",
+			Email:    "test@test.com",
+			Age:      20,
+			Profiles: []models.PlayerProfile{},
+		}
+
+		// Attempt to update the non-existent user
+		err := repo.UpdateUser(nonExistentUser)
+		require.Error(t, err, "Expected error when updating non-existent user")
+	})
 }
 
 func TestUserRepositoryImpl_DeleteUser(t *testing.T) {
 
-	// Setup
-	db := testutils.SetupTestDB(&models.User{}, &models.PlayerProfile{})
-	defer func() {
-		sqlDB, _ := db.DB()
-		sqlDB.Close()
-	}()
+	t.Run("DeleteUser_Success", func(t *testing.T) {
+		// Setup
+		db := testutils.SetupTestDB(&models.User{}, &models.PlayerProfile{})
+		defer func() {
+			sqlDB, _ := db.DB()
+			sqlDB.Close()
+		}()
 
-	// Create repository instance
-	repo := NewUserRepositoryImpl(db)
+		// Create repository instance
+		repo := NewUserRepositoryImpl(db)
 
-	// Define user to be created
-	user := &models.User{
-		UserName: "test",
-		PassWord: "test",
-		Email:    "test@test.com",
-		Age:      20,
-		Profiles: []models.PlayerProfile{},
-	}
+		// Define user to be created
+		user := &models.User{
+			UserName: "test",
+			PassWord: "test",
+			Email:    "test@test.com",
+			Age:      20,
+			Profiles: []models.PlayerProfile{},
+		}
 
-	// Attempt to create user
-	require.NoError(t, repo.CreateUser(user), "Error creating user")
+		// Attempt to create user
+		require.NoError(t, repo.CreateUser(user), "Error creating user")
 
-	// Verify user was created
-	require.NotZero(t, user.UserID, "User ID is zero")
+		// Verify user was created
+		require.NotZero(t, user.UserID, "User ID is zero")
 
-	// Attempt to delete user
-	require.NoError(t, repo.DeleteUser(user.UserID), "Error deleting user")
+		// Attempt to delete user
+		require.NoError(t, repo.DeleteUser(user.UserID), "Error deleting user")
 
-	// Verify user was deleted
-	var dbUser models.User
-	require.Error(t, db.Preload("Profiles").First(&dbUser, "user_name = ?", user.UserName).Error, "Error getting user")
+		// Verify user was deleted
+		var dbUser models.User
+		require.Error(t, db.Preload("Profiles").First(&dbUser, "user_name = ?", user.UserName).Error, "Error getting user")
 
-	// Assertions
-	require.Equal(t, models.User{}, dbUser, "User is not empty")
+		// Assertions
+		require.Equal(t, models.User{}, dbUser, "User is not empty")
+	})
+
+	t.Run("DeleteUser_NotFound", func(t *testing.T) {
+		// Setup
+		db := testutils.SetupTestDB(&models.User{}, &models.PlayerProfile{})
+		defer func() {
+			sqlDB, _ := db.DB()
+			sqlDB.Close()
+		}()
+
+		repo := NewUserRepositoryImpl(db)
+
+		// Attempt to delete a non-existent user
+		err := repo.DeleteUser(9999) // Assume 9999 is a non-existent ID
+		require.Error(t, err, "Expected error when deleting non-existent user")
+	})
 }

27874025dbb7b1747dc8e87f5f5bd6c5c49e87a2
dieg0code <diegoobando20@gmail.com>
2024-07-19 17:26:13 -0400

    added error test cases

--- src/repository/impl/player_profile_repository_impl_test.go
+++ src/repository/impl/player_profile_repository_impl_test.go
@@ -26,6 +26,17 @@ var (
 	}
 )
 
+func TestNewPlayerProfileRepositoryImpl(t *testing.T) {
+	db := testutils.SetupTestDB(&models.PlayerProfile{}, &models.User{})
+	defer func() {
+		sqlDB, _ := db.DB()
+		sqlDB.Close()
+	}()
+
+	playerRepo := NewPlayerProfileRepositoryImpl(db)
+	require.NotNil(t, playerRepo, "PlayerProfileRepositoryImpl is nil")
+}
+
 func TestPlayerProfileRespositoryImpl_CreatePlayerProfile(t *testing.T) {
 
 	t.Run("CreatePlayerProfile_Success", func(t *testing.T) {
@@ -84,6 +95,25 @@ func TestPlayerProfileRespositoryImpl_CreatePlayerProfile(t *testing.T) {
 		require.Error(t, err, "Expected error creating duplicate player profile")
 	})
 
+	// t.Run("CreatePlayerProfile_Error", func(t *testing.T) {
+	// 	db := testutils.SetupTestDB(&models.PlayerProfile{}, &models.User{})
+	// 	defer func() {
+	// 		sqlDB, _ := db.DB()
+	// 		sqlDB.Close()
+	// 	}()
+
+	// 	playerRepo := NewPlayerProfileRepositoryImpl(db)
+
+	// 	// Attempt to create Player with invalid data
+	// 	// Attempt to create Player with invalid data
+	// 	invalidProfile := &models.PlayerProfile{
+	// 		Nickname: "",
+	// 	}
+
+	// 	err := playerRepo.CreatePlayerProfile(invalidProfile)
+	// 	require.Error(t, err, "Expected error creating player profile with invalid data")
+	// })
+
 }
 
 func TestPlayerProfileRespositoryImpl_GetPlayerProfile(t *testing.T) {
@@ -130,6 +160,20 @@ func TestPlayerProfileRespositoryImpl_GetPlayerProfile(t *testing.T) {
 		// Attempt to get player profile
 		_, err := playerRepo.GetPlayerProfile(1)
 		require.Error(t, err, "Expected error getting player profile")
+	})
+
+	t.Run("GetPlayerProfile_Error", func(t *testing.T) {
+		db := testutils.SetupTestDB(&models.PlayerProfile{}, &models.User{})
+		defer func() {
+			sqlDB, _ := db.DB()
+			sqlDB.Close()
+		}()
+
+		playerRepo := NewPlayerProfileRepositoryImpl(db)
+
+		// Attempt to get player profile with invalid data
+		_, err := playerRepo.GetPlayerProfile(99999)
+		require.Error(t, err, "Expected error getting player profile with invalid data")
 	})
 
 }
@@ -184,6 +228,25 @@ func TestPlayerProfileRespositoryImpl_UpdatePlayerProfile(t *testing.T) {
 		require.Error(t, err, "Expected error updating player profile")
 	})
 
+	t.Run("UpdatePlayerProfile_Error", func(t *testing.T) {
+		db := testutils.SetupTestDB(&models.PlayerProfile{}, &models.User{})
+		defer func() {
+			sqlDB, _ := db.DB()
+			sqlDB.Close()
+		}()
+
+		playerRepo := NewPlayerProfileRepositoryImpl(db)
+
+		// Attempt to update player profile with invalid data
+		invalidPlayer := &models.PlayerProfile{
+			PlayerProfileID: 1,
+			Nickname:        "",
+		}
+
+		err := playerRepo.UpdatePlayerProfile(invalidPlayer)
+		require.Error(t, err, "Expected error updating player profile with invalid data")
+	})
+
 }
 
 func TestPlayerProfileRespositoryImpl_DeletePlayerProfile(t *testing.T) {
@@ -232,6 +295,20 @@ func TestPlayerProfileRespositoryImpl_DeletePlayerProfile(t *testing.T) {
 		require.Error(t, err, "Expected error deleting player profile")
 	})
 
+	t.Run("DeletePlayerProfile_Error", func(t *testing.T) {
+		db := testutils.SetupTestDB(&models.PlayerProfile{}, &models.User{})
+		defer func() {
+			sqlDB, _ := db.DB()
+			sqlDB.Close()
+		}()
+
+		playerRepo := NewPlayerProfileRepositoryImpl(db)
+
+		// Attempt to delete player profile with invalid data
+		err := playerRepo.DeletePlayerProfile(999999)
+		require.Error(t, err, "Expected error deleting player profile with invalid data")
+	})
+
 }
 
 func TestPlayerProfileRespositoryImpl_CheckPlayerProfileExists(t *testing.T) {
@@ -276,5 +353,20 @@ func TestPlayerProfileRespositoryImpl_CheckPlayerProfileExists(t *testing.T) {
 		exists, err := playerRepo.CheckPlayerProfileExists(1)
 		require.NoError(t, err, "Error checking if player exists")
 		require.False(t, exists, "Player exists")
+	})
+
+	t.Run("CheckPlayerProfileExists_Error", func(t *testing.T) {
+		db := testutils.SetupTestDB(&models.PlayerProfile{}, &models.User{})
+		defer func() {
+			sqlDB, _ := db.DB()
+			sqlDB.Close()
+		}()
+
+		playerRepo := NewPlayerProfileRepositoryImpl(db)
+
+		// Check if Player exists with invalid data
+		exists, err := playerRepo.CheckPlayerProfileExists(9999)
+		require.NoError(t, err, "No error expected for non-existent player profile check")
+		require.False(t, exists, "Expected non-existent player profile")
 	})
 }

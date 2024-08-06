51172cd1f1541074b4f43dbaf0cdb23ae6b8370c
dieg0code <diegoobando20@gmail.com>
2024-08-06 19:56:40 -0400

    fix some tf configs

--- terraform/storage.tf
+++ terraform/storage.tf
@@ -5,13 +5,23 @@ resource "aws_s3_bucket" "lb_logs" {
     Name        = "${var.app_name}-${var.environment}-lb-logs"
     Environment = var.environment
   }
-  
+
+}
+
+resource "aws_s3_bucket_public_access_block" "lb_logs_public_access_block" {
+  bucket = aws_s3_bucket.lb_logs.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
 }
 
 resource "aws_s3_bucket_versioning" "lb_bucket_versioning" {
   bucket = aws_s3_bucket.lb_logs.id
   versioning_configuration {
-    status = "Enabled"
+    status     = "Enabled"
+    mfa_delete = "Enabled"
   }
 }
 
